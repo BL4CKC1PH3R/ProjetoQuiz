51c8f55e0e12640956deaf1adf8796109623a978
Muud12 <gu.bat98@gmail.com>
2015-09-24 18:36:04 -0300

    Quiz O cortiço
    Quiz O cortiço
    2º F

---
+++ Quizz/Quizz/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Quizz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblPergunta = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAgain = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblC);
+            this.groupBox1.Controls.Add(this.lblB);
+            this.groupBox1.Controls.Add(this.lblA);
+            this.groupBox1.Controls.Add(this.btn_ok);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 222);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(746, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resposta";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.Location = new System.Drawing.Point(41, 140);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(0, 16);
+            this.lblC.TabIndex = 6;
+            this.lblC.Click += new System.EventHandler(this.lblC_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(43, 85);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 16);
+            this.lblB.TabIndex = 5;
+            this.lblB.Click += new System.EventHandler(this.lblB_Click);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(43, 32);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 16);
+            this.lblA.TabIndex = 4;
+            this.lblA.Click += new System.EventHandler(this.lblA_Click);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(654, 120);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(85, 48);
+            this.btn_ok.TabIndex = 3;
+            this.btn_ok.Text = "Próxima Questão";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(6, 138);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(35, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "C";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(35, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblPergunta);
+            this.groupBox2.Location = new System.Drawing.Point(9, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(746, 213);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Questão";
+            // 
+            // lblPergunta
+            // 
+            this.lblPergunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPergunta.Location = new System.Drawing.Point(6, 19);
+            this.lblPergunta.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.lblPergunta.Multiline = true;
+            this.lblPergunta.Name = "lblPergunta";
+            this.lblPergunta.ReadOnly = true;
+            this.lblPergunta.Size = new System.Drawing.Size(734, 179);
+            this.lblPergunta.TabIndex = 10;
+            this.lblPergunta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(283, 120);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(263, 24);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Você Acertou 10 questões!";
+            this.lblResult.Visible = false;
+            // 
+            // btnAgain
+            // 
+            this.btnAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgain.Location = new System.Drawing.Point(336, 164);
+            this.btnAgain.Name = "btnAgain";
+            this.btnAgain.Size = new System.Drawing.Size(157, 54);
+            this.btnAgain.TabIndex = 3;
+            this.btnAgain.Text = "Refazer Teste";
+            this.btnAgain.UseVisualStyleBackColor = true;
+            this.btnAgain.Visible = false;
+            this.btnAgain.Click += new System.EventHandler(this.btnAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 410);
+            this.Controls.Add(this.btnAgain);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz : O Cortiço";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox lblPergunta;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAgain;
+    }
+}
+
---
+++ Quizz/Quizz/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace Quizz
+{
+    public partial class Form1 : Form
+    {
+        int questao = 0;
+        int acertos = 0;
+        string alternativa = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            /*
+            MessageBox.Show(alternativa);*/
+            //limpaAlternativas();
+            //carregaAlernativas(1);
+            limpaQuiz();
+        }
+
+        private void recebeAlternativa(RadioButton rdoButton)
+        {
+            if (rdoButton.Checked)
+            {
+                alternativa = rdoButton.Text;
+            }
+        }
+
+   
+       
+
+        private void carregaQuiz(int id)
+        {
+            MySqlConnection oConn = new MySqlConnection("Server=localhost;Database=quiz;Uid=root;Pwd=;");
+            MySqlConnection oConn2 = new MySqlConnection("Server=localhost;Database=quiz;Uid=root;Pwd=;");
+            oConn.Open();
+            oConn2.Open();
+
+            MySqlCommand oCmd = new MySqlCommand("SELECT * FROM PERGUNTAS WHERE ID = "+ id, oConn);
+            MySqlCommand oCmd2 = new MySqlCommand("SELECT * FROM RESPOSTAS WHERE ID_PERGUNTA = "+ id, oConn2);
+
+            MySqlDataReader oDr = oCmd2.ExecuteReader();
+            MySqlDataReader oDr2 = oCmd.ExecuteReader();
+            
+            while (oDr2.Read())
+            {
+                if (lblPergunta.Text == "")
+                {
+                    lblPergunta.Text = oDr2["PERGUNTA"].ToString();
+                }
+            }
+
+            while (oDr.Read())
+            {
+                if (lblA.Text == "")
+                {
+                    lblA.Text = oDr["RESPOSTA"].ToString();
+                    oDr.Read();
+                }
+
+
+
+                if (lblB.Text == "")
+                {
+                    lblB.Text = oDr["RESPOSTA"].ToString();
+                    oDr.Read();
+                }
+
+                if (lblC.Text == "")
+                {
+                    lblC.Text = oDr["RESPOSTA"].ToString();
+                    oDr.Read();
+                }
+            }
+            oConn.Close();
+        }
+
+        private void limpaQuiz()
+        {
+            if (questao <= 10)
+            {
+                checaQuiz();
+                questao++;
+                lblA.Text = "";
+                lblB.Text = "";
+                lblC.Text = "";
+                lblPergunta.Text = "";
+                carregaQuiz(questao);
+                
+            }
+
+            if(questao == 11)
+            {
+                groupBox2.Visible = false;
+                groupBox1.Visible = false;
+                lblResult.Text = "Você acertou " + acertos + " questões!";
+                lblResult.Visible = true;
+                btnAgain.Visible = true;
+            }
+
+
+            
+        }
+
+
+        private void checaQuiz()
+        {
+            recebeAlternativa(radioButton1);
+            recebeAlternativa(radioButton2);
+            recebeAlternativa(radioButton3);
+
+            MySqlConnection oConn = new MySqlConnection("Server=localhost;Database=quiz;Uid=root;Pwd=;");
+            oConn.Open();
+            MySqlCommand oCmd = new MySqlCommand("SELECT * FROM RESPOSTAS WHERE ID_PERGUNTA = " + questao +" AND LETRA= '" + alternativa +"'", oConn);
+            MySqlDataReader oDr = oCmd.ExecuteReader();
+
+            while (oDr.Read())
+            {
+                if (oDr.GetBoolean("CORRETA")  == true)
+                {
+                    acertos++;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            questao = 1;
+            carregaQuiz(questao);
+            lblPergunta.ReadOnly = true;
+        }
+
+        private void lblA_Click(object sender, EventArgs e)
+        {
+            radioButton1.Select();
+        }
+
+        private void lblB_Click(object sender, EventArgs e)
+        {
+            radioButton2.Select();
+        }
+
+        private void lblC_Click(object sender, EventArgs e)
+        {
+            radioButton3.Select();
+        }
+
+        private void btnAgain_Click(object sender, EventArgs e)
+        {
+            questao = 1;
+            acertos = 0;
+            carregaQuiz(questao);
+            groupBox2.Visible = true;
+            groupBox1.Visible = true;
+            lblResult.Visible = false;
+            btnAgain.Visible = false;
+        }
+    }
+}
